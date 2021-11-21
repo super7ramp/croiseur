b1af248b82ecba2ef32a2e90a245941c3d20dd27
Antoine Belvire <antoine.belvire@opensuse.org>
2021-11-21 22:35:14 +0100

    Clean obsolete files

--- com.gitlab.super7ramp.crosswords.db.api/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * Crossword Database Module.
- */
-module com.gitlab.super7ramp.crosswords.db.api {
-    exports com.gitlab.super7ramp.crosswords.db.api;
-    exports com.gitlab.super7ramp.crosswords.db.api.spi;
-}
