ff9b947428aaea98b51c0d21c09de8ad11e115e7
Antoine Belvire <antoine.belvire@opensuse.org>
2023-04-20 22:09:37 +0200

    dictionary-xml: Force Unix line ending in test
    XML Transformer used in tests to format XML output uses platform's
    line endings (i.e. "\r\n" on Windows, "\n" on Linux). Let's force Unix
    line ending for reproducibility.

--- croiseur-dictionary/croiseur-dictionary-xml-codec/src/test/java/com/gitlab/super7ramp/croiseur/dictionary/xml/codec/DictionaryWriterTest.java
+++ croiseur-dictionary/croiseur-dictionary-xml-codec/src/test/java/com/gitlab/super7ramp/croiseur/dictionary/xml/codec/DictionaryWriterTest.java
@@ -33,7 +33,7 @@ final class DictionaryWriterTest {
             t.setOutputProperty(OutputKeys.INDENT, "yes");
             final Writer out = new StringWriter();
             t.transform(new StreamSource(new StringReader(xml)), new StreamResult(out));
-            return out.toString();
+            return out.toString().replaceAll("\r\n", "\n"); // Force Unix line ending
         } catch (final TransformerException e) {
             throw new AssertionError("Error in test code when formatting XML output", e);
         }
@@ -47,9 +47,9 @@ final class DictionaryWriterTest {
                 .addName(Locale.ENGLISH, "Dictionary example")
                 .addName(Locale.FRANCE, "Exemple de dictionnaire")
                 .addDescription(Locale.ENGLISH, "An example of XML " +
-                        "dictionary. Useful for discovery and testing purposes.")
+                                                "dictionary. Useful for discovery and testing purposes.")
                 .addDescription(Locale.FRANCE, "Un exemple de dictionnaire. Utile à des fins de " +
-                        "découverte et de tests.")
+                                               "découverte et de tests.")
                 .build();
         final List<String> words = List.of("Hello", "World");
         final Dictionary dictionary = new Dictionary(header, words);
@@ -57,19 +57,19 @@ final class DictionaryWriterTest {
         DictionaryWriter.write(outputStream, dictionary);
 
         assertEquals("""
-                <?xml version="1.0" encoding="UTF-8"?>\
-                <tns:dictionary xmlns:tns="http://www.example.org/dictionary" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.example.org/dictionary dictionary.xsd">
-                    <locale>en</locale>
-                    <name>Dictionary example</name>
-                    <name xml:lang="fr-FR">Exemple de dictionnaire</name>
-                    <description>An example of XML dictionary. Useful for discovery and testing purposes.</description>
-                    <description xml:lang="fr-FR">Un exemple de dictionnaire. Utile à des fins de découverte et de tests.</description>
-                    <words>
-                        <word>Hello</word>
-                        <word>World</word>
-                    </words>
-                </tns:dictionary>
-                """, formatXml(outputStream.toString()));
+                     <?xml version="1.0" encoding="UTF-8"?>\
+                     <tns:dictionary xmlns:tns="http://www.example.org/dictionary" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.example.org/dictionary dictionary.xsd">
+                         <locale>en</locale>
+                         <name>Dictionary example</name>
+                         <name xml:lang="fr-FR">Exemple de dictionnaire</name>
+                         <description>An example of XML dictionary. Useful for discovery and testing purposes.</description>
+                         <description xml:lang="fr-FR">Un exemple de dictionnaire. Utile à des fins de découverte et de tests.</description>
+                         <words>
+                             <word>Hello</word>
+                             <word>World</word>
+                         </words>
+                     </tns:dictionary>
+                     """, formatXml(outputStream.toString()));
     }
 
 }
