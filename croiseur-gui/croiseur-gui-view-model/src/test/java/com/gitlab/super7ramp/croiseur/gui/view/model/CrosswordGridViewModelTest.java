070bc1179784521f1b32e53be1a334b3edbce51f
Antoine Belvire <antoine.belvire@opensuse.org>
2023-05-23 21:13:18 +0200

    gui: Add grid schemas in tests
    To be able to see faster what each test does.

--- croiseur-gui/croiseur-gui-view-model/src/test/java/com/gitlab/super7ramp/croiseur/gui/view/model/CrosswordGridViewModelTest.java
+++ croiseur-gui/croiseur-gui-view-model/src/test/java/com/gitlab/super7ramp/croiseur/gui/view/model/CrosswordGridViewModelTest.java
@@ -18,6 +18,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests for {@link CrosswordGridViewModel}.
+ * <p>
+ * Legend of grid schemas:
+ * <ul>
+ *     <li>{@code |    |}: An empty cell
+ *     <li>{@code | A  |}: A cell containing the letter 'A'
+ *     <li>{@code | #  |}: A shaded cell
+ *     <li>{@code |( ) |}: An empty cell part of the selected slot
+ *     <li>{@code |(A) |}: A cell containing the letter 'A' and part of the selected slot
+ *     <li>{@code |( )'|}: A focused cell part of the selected slot
+ *     <li>{@code |(A)'|}: A focused cell containing the letter 'A' and part of the selected slot
+ *     <li>{@code | # '|}: A focused shaded cell
+ * </ul>
  */
 final class CrosswordGridViewModelTest {
 
@@ -29,12 +41,18 @@ final class CrosswordGridViewModelTest {
         crosswordGridViewModel = CrosswordGridViewModel.newGrid();
     }
 
+    /**
+     * Checks that default model is empty.
+     */
     @Test
     void dimensions_empty() {
         assertEquals(0, crosswordGridViewModel.columnCount());
         assertEquals(0, crosswordGridViewModel.rowCount());
     }
 
+    /**
+     * Checks that adding a row also updates the column count.
+     */
     @Test
     void dimensions_1x1() {
         // First row implicitly adds a first column and vice-versa
@@ -44,6 +62,16 @@ final class CrosswordGridViewModelTest {
         assertEquals(1, crosswordGridViewModel.rowCount());
     }
 
+    /**
+     * Checks column/row additions with a 2x3 grid without any filled/shaded cells.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |    |    |
+     *     |    |    |
+     *     |    |    |
+     * </pre>
+     */
     @Test
     void dimensions_2x3() {
         // First column implicitly adds a first row and vice-versa
@@ -56,6 +84,16 @@ final class CrosswordGridViewModelTest {
         assertEquals(3, crosswordGridViewModel.rowCount());
     }
 
+    /**
+     * Checks column deletion with a 1x3 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |    |
+     *     |    |
+     *     |    |
+     * </pre>
+     */
     @Test
     void deleteLastColumn_noMoreColumn() {
         crosswordGridViewModel.addColumn();
@@ -70,6 +108,14 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.boxesProperty().isEmpty());
     }
 
+    /**
+     * Checks row deletion with a 3x1 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |    |    |    |
+     * </pre>
+     */
     @Test
     void deleteLastRow_noMoreRow() {
         crosswordGridViewModel.addRow();
@@ -84,6 +130,9 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.boxesProperty().isEmpty());
     }
 
+    /**
+     * Verifies that direct modification of the map of boxes is rejected.
+     */
     @Test
     void boxes_unmodifiable() {
         assertThrows(UnsupportedOperationException.class,
@@ -91,6 +140,16 @@ final class CrosswordGridViewModelTest {
                                                  .put(at(0, 0), new CrosswordBoxViewModel()));
     }
 
+    /**
+     * Checks horizontal selection with a 2x3 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |( )'|( ) |
+     *     |    |    |
+     *     |    |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_newHorizontalSelection() {
         crosswordGridViewModel.addColumn();
@@ -104,6 +163,14 @@ final class CrosswordGridViewModelTest {
                      crosswordGridViewModel.currentSlotPositionsProperty());
     }
 
+    /**
+     * Checks horizontal selection between shaded cells with a 6x1 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |    | #  |( )'|( ) | #  |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_newHorizontalSelectionBetweenShaded() {
         for (int i = 0; i < 6; i++) {
@@ -118,6 +185,19 @@ final class CrosswordGridViewModelTest {
                      crosswordGridViewModel.currentSlotPositionsProperty());
     }
 
+    /**
+     * Checks that horizontal selection between shaded cells with a 6x1 grid grows when shaded cells
+     * are removed.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |    | #  |( )'|( ) | #  |    |
+     * </pre>
+     * Grid schema (after):
+     * <pre>
+     *     |( ) |( ) |( )'|( ) |( ) |( ) |
+     * </pre>
+     */
     @Test
     void selectedSlot_horizontalSelectionGrows() {
         selectedSlot_newHorizontalSelectionBetweenShaded();
@@ -130,6 +210,16 @@ final class CrosswordGridViewModelTest {
                      crosswordGridViewModel.currentSlotPositionsProperty());
     }
 
+    /**
+     * Checks vertical selection with a 2x3 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |( )'|    |
+     *     |( ) |    |
+     *     |( ) |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_newVerticalSelection() {
         crosswordGridViewModel.addColumn();
@@ -144,6 +234,19 @@ final class CrosswordGridViewModelTest {
                      crosswordGridViewModel.currentSlotPositionsProperty());
     }
 
+    /**
+     * Checks vertical selection between shaded cells with a 1x6 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |    |
+     *     | #  |
+     *     |( )'|
+     *     |( ) |
+     *     | #  |
+     *     |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_newVerticalSelectionBetweenShaded() {
         for (int i = 0; i < 6; i++) {
@@ -159,6 +262,29 @@ final class CrosswordGridViewModelTest {
                      crosswordGridViewModel.currentSlotPositionsProperty());
     }
 
+    /**
+     * Checks vertical selection between shaded cells with a 1x6 grid grows when shaded cells are
+     * removed
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |    |
+     *     | #  |
+     *     |( )'|
+     *     |( ) |
+     *     | #  |
+     *     |    |
+     * </pre>
+     * Grid schema (after):
+     * <pre>
+     *     |( ) |
+     *     |( ) |
+     *     |( )'|
+     *     |( ) |
+     *     |( ) |
+     *     |( ) |
+     * </pre>
+     */
     @Test
     void selectedSlot_verticalSelectionGrows() {
         selectedSlot_newVerticalSelectionBetweenShaded();
@@ -170,6 +296,9 @@ final class CrosswordGridViewModelTest {
                      crosswordGridViewModel.currentSlotPositionsProperty().get());
     }
 
+    /**
+     * Checks that when no box is focused, no slot is selected.
+     */
     @Test
     void selectedSlot_noCurrentBox() {
         crosswordGridViewModel.addColumn();
@@ -180,6 +309,22 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.currentSlotPositionsProperty().isEmpty());
     }
 
+    /**
+     * Checks that selected horizontal slot is correctly cleared when the row it belongs to is
+     * deleted.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |    |    |
+     *     |    |    |
+     *     |( )'|( ) |
+     * </pre>
+     * Grid schema (after):
+     * <pre>
+     *     |    |    |
+     *     |    |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_currentBoxDeleted_horizontal() {
         // Set last line as current slot of a 2x3 grid
@@ -199,6 +344,23 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.currentSlotPositionsProperty().isEmpty());
     }
 
+    /**
+     * Checks that selected vertical slot is correctly cleared when the column it belongs to is
+     * deleted.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |    |( )'|
+     *     |    |( ) |
+     *     |    |( ) |
+     * </pre>
+     * Grid schema (after):
+     * <pre>
+     *     |    |
+     *     |    |
+     *     |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_currentBoxDeleted_vertical() {
         // Set last column as current slot of a 2x3 grid
@@ -219,6 +381,16 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.currentSlotPositionsProperty().isEmpty());
     }
 
+    /**
+     * Checks that no selected slot is present when current box is a shaded box.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |    |    |
+     *     | # '|    |
+     *     |    |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_currentBoxInitializedWithShaded() {
         crosswordGridViewModel.addColumn();
@@ -232,6 +404,23 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.currentSlotPositionsProperty().isEmpty());
     }
 
+    /**
+     * Checks that selected slot is correctly cleared when current box moves from a non-shaded
+     * box to a shaded box.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |( )'|( ) |
+     *     | #  |    |
+     *     |    |    |
+     * </pre>
+     * Grid schema (after):
+     * <pre>
+     *     |    |    |
+     *     | #' |    |
+     *     |    |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_currentBoxChangedWithShaded() {
         crosswordGridViewModel.addColumn();
@@ -248,6 +437,22 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.currentSlotPositionsProperty().isEmpty());
     }
 
+    /**
+     * Checks that selected slot is correctly cleared when current non-shaded box becomes shaded.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |( )'|( ) |
+     *     |    |    |
+     *     |    |    |
+     * </pre>
+     * Grid schema (after):
+     * <pre>
+     *     | #' |    |
+     *     |    |    |
+     *     |    |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_currentBoxMutatedFromBlankToShaded() {
         crosswordGridViewModel.addColumn();
@@ -263,22 +468,47 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.currentSlotPositionsProperty().isEmpty());
     }
 
+    /**
+     * Checks that selected slot is correctly created when current shaded box becomes non-shaded.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     | #' |    |
+     *     |    |    |
+     *     |    |    |
+     * </pre>
+     * Grid schema (after):
+     * <pre>
+     *     |( )'|( ) |
+     *     |    |    |
+     *     |    |    |
+     * </pre>
+     */
     @Test
     void selectedSlot_currentBoxMutatedFromShadedToBlank() {
         crosswordGridViewModel.addColumn();
         crosswordGridViewModel.addColumn();
         crosswordGridViewModel.addRow();
         crosswordGridViewModel.addRow();
-        crosswordGridViewModel.boxesProperty().get(at(0, 1)).shade();
-        crosswordGridViewModel.currentBoxPosition(at(0, 1));
+        crosswordGridViewModel.boxesProperty().get(at(0, 0)).shade();
+        crosswordGridViewModel.currentBoxPosition(at(0, 0));
         assertTrue(crosswordGridViewModel.currentSlotPositionsProperty().isEmpty());
 
-        crosswordGridViewModel.boxesProperty().get(at(0, 1)).lighten();
+        crosswordGridViewModel.boxesProperty().get(at(0, 0)).lighten();
 
-        assertEquals(List.of(at(0, 1), at(1, 1)),
+        assertEquals(List.of(at(0, 0), at(1, 0)),
                      crosswordGridViewModel.currentSlotPositionsProperty());
     }
 
+    /**
+     * Checks a fully filled current slot content on a 3x1 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |(A)'|(B) |(C) |
+     * </pre>
+     * Expected content is "ABC".
+     */
     @Test
     void selectedSlotContent_filled() {
         crosswordGridViewModel.addColumn();
@@ -293,6 +523,15 @@ final class CrosswordGridViewModelTest {
         assertEquals("ABC", crosswordGridViewModel.currentSlotContent());
     }
 
+    /**
+     * Checks a partially filled current slot content on a 3x1 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |(A)'|( ) |(C) |
+     * </pre>
+     * Expected content is "A.C".
+     */
     @Test
     void selectedSlotContent_partiallyFilled() {
         crosswordGridViewModel.addColumn();
@@ -306,6 +545,15 @@ final class CrosswordGridViewModelTest {
         assertEquals("A.C", crosswordGridViewModel.currentSlotContent());
     }
 
+    /**
+     * Checks a blank current slot content on a 3x1 grid.
+     * <p>
+     * Grid schema:
+     * <pre>
+     *     |( )'|( ) |( ) |
+     * </pre>
+     * Expected content is "...".
+     */
     @Test
     void selectedSlotContent_onlyBlanks() {
         crosswordGridViewModel.addColumn();
@@ -317,6 +565,9 @@ final class CrosswordGridViewModelTest {
         assertEquals("...", crosswordGridViewModel.currentSlotContent());
     }
 
+    /**
+     * Checks that by default no selected content is present.
+     */
     @Test
     void selectedSlotContent_noSelection() {
         crosswordGridViewModel.addColumn();
@@ -326,6 +577,21 @@ final class CrosswordGridViewModelTest {
         assertTrue(crosswordGridViewModel.currentSlotContent().isEmpty());
     }
 
+    /**
+     * Checks that slot content is updated when grid boxes are modified.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |(A)'|( ) |(C) |
+     * </pre>
+     * Content was "A.C".
+     * <p>
+     * Grid schema (after):
+     * <pre>
+     *     |(A)'|(B) |(C) |
+     * </pre>
+     * Expected content is "ABC".
+     */
     @Test
     void selectedSlotContent_contentChange() {
         selectedSlotContent_partiallyFilled();
@@ -335,6 +601,21 @@ final class CrosswordGridViewModelTest {
         assertEquals("ABC", crosswordGridViewModel.currentSlotContent());
     }
 
+    /**
+     * Checks that slot content is updated when grid structure is modified - here a column is added.
+     * <p>
+     * Grid schema (before):
+     * <pre>
+     *     |(A)'|(B) |(C) |
+     * </pre>
+     * Content was "ABC".
+     * <p>
+     * Grid schema (after):
+     * <pre>
+     *     |(A)'|(B) |(C) |( ) |
+     * </pre>
+     * Expected content is "ABC.".
+     */
     @Test
     void selectedSlotContent_contentGrows() {
         selectedSlotContent_filled();
