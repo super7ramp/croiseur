1f63b6b285793d6e0207724d07fe0a45a6e8fe12
Antoine Belvire <antoine.belvire@opensuse.org>
2023-06-20 21:34:43 +0200

    puzzle-xd: Implement grid writer

---
+++ croiseur-puzzle/croiseur-puzzle-codec-xd/src/main/java/com/gitlab/super7ramp/croiseur/puzzle/codec/xd/writer/XdGridWriter.java
@@ -0,0 +1,56 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Antoine Belvire
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.gitlab.super7ramp.croiseur.puzzle.codec.xd.writer;
+
+import com.gitlab.super7ramp.croiseur.puzzle.codec.xd.model.XdGrid;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * Encodes {@link XdGrid} to its textual representation.
+ */
+final class XdGridWriter {
+
+    /** Compares grid indexes by row then by column. */
+    private static final Comparator<XdGrid.Index> SORTED_BY_ROW_THEN_BY_COLUMN =
+            Comparator.comparingInt(XdGrid.Index::row).thenComparing(XdGrid.Index::column);
+
+    /**
+     * Constructs an instance.
+     */
+    XdGridWriter() {
+        // Nothing to do.
+    }
+
+    /**
+     * Writes the given grid to a string.
+     *
+     * @param grid the grid
+     * @return the written string
+     */
+    String write(final XdGrid grid) {
+        Objects.requireNonNull(grid);
+
+        final SortedMap<XdGrid.Index, String> boxes = new TreeMap<>(SORTED_BY_ROW_THEN_BY_COLUMN);
+        boxes.putAll(grid.filled());
+        grid.nonFilled().forEach(index -> boxes.put(index, "."));
+        grid.blocks().forEach(index -> boxes.put(index, "#"));
+        grid.spaces().forEach(index -> boxes.put(index, "_"));
+        final int lastColumn = boxes.lastKey().column();
+
+        final StringBuilder sb = new StringBuilder();
+        boxes.forEach((index, content) -> {
+            sb.append(content);
+            if (index.column() == lastColumn) {
+                sb.append('\n');
+            }
+        });
+        return sb.toString();
+    }
+}
