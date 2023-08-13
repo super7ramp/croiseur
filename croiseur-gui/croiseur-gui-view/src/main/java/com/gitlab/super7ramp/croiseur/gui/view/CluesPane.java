ada2bb07a6183f7017bb65b28238aeaa36f06b22
Antoine Belvire <antoine.belvire@opensuse.org>
2023-08-13 15:30:52 +0200

    gui: Fix spurious previous slot selection upon box shading

--- croiseur-gui/croiseur-gui-view/src/main/java/com/gitlab/super7ramp/croiseur/gui/view/CluesPane.java
+++ croiseur-gui/croiseur-gui-view/src/main/java/com/gitlab/super7ramp/croiseur/gui/view/CluesPane.java
@@ -7,13 +7,16 @@ package com.gitlab.super7ramp.croiseur.gui.view;
 
 import com.gitlab.super7ramp.croiseur.gui.view.control.cell.TextFieldListCell;
 import com.gitlab.super7ramp.croiseur.gui.view.model.ClueViewModel;
+import javafx.beans.Observable;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionModel;
 import javafx.scene.layout.HBox;
 import javafx.util.StringConverter;
 
@@ -145,60 +148,173 @@ public final class CluesPane extends HBox {
      * Initializes across clue list view bindings.
      */
     private void initializeAcrossClueListView() {
+        // pure appearance
         acrossClueListView.prefHeightProperty()
                           .bind(acrossClues.sizeProperty().multiply(CELL_HEIGHT));
 
+        // model -> view
         acrossClueListView.setCellFactory(l -> new TextFieldListCell<>(USER_CLUE_STRING_CONVERTER,
                                                                        SYSTEM_CLUE_STRING_CONVERTER));
+        acrossClues.addListener(this::clearAcrossViewSelectionUponItemDeletion);
         acrossClueListView.setItems(acrossClues);
+        selectedAcrossClueIndex.addListener(
+                this::updateViewAcrossSelectionUponModelSelectionIndexChange);
 
         // view -> model
-        final var acrossClueSelectionModel = acrossClueListView.getSelectionModel();
-        acrossClueSelectionModel.selectedIndexProperty().addListener(observable -> {
-            selectedAcrossClueIndex.set(acrossClueSelectionModel.getSelectedIndex());
-            if (selectedAcrossClueIndex.get() >= 0) {
-                downClueListView.getSelectionModel().clearSelection();
-            }
-        });
+        acrossClueListView.getSelectionModel().selectedIndexProperty().addListener(
+                this::updateAcrossModelSelectionUponViewSelectionIndexChange);
 
-        // model -> view
-        selectedAcrossClueIndex.addListener(observable -> {
-            final int index = selectedAcrossClueIndex.get();
-            if (index < 0) {
-                acrossClueSelectionModel.clearSelection();
-            } else {
-                acrossClueSelectionModel.select(index);
-            }
-        });
     }
 
     /**
      * Initializes down clue list view bindings.
      */
     private void initializeDownClueListView() {
+        // Pure appearance
         downClueListView.prefHeightProperty().bind(downClues.sizeProperty().multiply(CELL_HEIGHT));
 
+        // model -> view
         downClueListView.setCellFactory(l -> new TextFieldListCell<>(USER_CLUE_STRING_CONVERTER,
                                                                      SYSTEM_CLUE_STRING_CONVERTER));
+        downClues.addListener(this::clearDownSelectionUponItemDeletion);
         downClueListView.setItems(downClues);
+        selectedDownClueIndex.addListener(
+                this::updateViewDownSelectionUponModelSelectionIndexChange);
 
         // view -> model
-        final var downClueSelectionModel = downClueListView.getSelectionModel();
-        downClueSelectionModel.selectedIndexProperty().addListener(observable -> {
-            selectedDownClueIndex.set(downClueSelectionModel.getSelectedIndex());
-            if (selectedDownClueIndex.get() >= 0) {
-                acrossClueListView.getSelectionModel().clearSelection();
-            }
-        });
+        downClueListView.getSelectionModel().selectedIndexProperty().addListener(
+                this::updateDownModelSelectionUponViewSelectionIndexChange);
+    }
 
-        // model -> view
-        selectedDownClueIndex.addListener(observable -> {
-            final int index = selectedDownClueIndex.get();
-            if (index < 0) {
-                downClueSelectionModel.clearSelection();
-            } else {
-                downClueSelectionModel.select(index);
+    /**
+     * Clears view across clue selection upon selected item deletion from model.
+     *
+     * @param c the model change
+     * @see #clearViewSelectionUponItemDeletion
+     */
+    private void clearAcrossViewSelectionUponItemDeletion(
+            final ListChangeListener.Change<? extends ClueViewModel> c) {
+        clearViewSelectionUponItemDeletion(c, acrossClueListView.getSelectionModel());
+    }
+
+    /**
+     * Clears view down clue selection upon selected item deletion from model.
+     *
+     * @param c the model change
+     * @see #clearViewSelectionUponItemDeletion
+     */
+    private void clearDownSelectionUponItemDeletion(
+            final ListChangeListener.Change<? extends ClueViewModel> c) {
+        clearViewSelectionUponItemDeletion(c, downClueListView.getSelectionModel());
+    }
+
+    /**
+     * Clears view selection upon selected item deletion from model.
+     * <p>
+     * {@link ListView} has a non-trivial and non-customizable strategy to update the selection upon
+     * model modification. In particular, if the selected item is deleted, it will try to shift the
+     * selection to the previous row. This behaviour is <em>not</em> desirable here: Deleting a clue
+     * does <em>not</em> mean we want to select the previous clue.
+     * <p>
+     * This behaviour is particularly visible when binding slots selection to clue selection
+     * bi-directionally: Shading a box may delete a slot, which would delete a clue, which would
+     * make ListView select the previous clue, which would trigger the selection of the previous
+     * slot, which is definitely not desirable.
+     * <p>
+     * This method forces the list view to clear its selection when the selected item is deleted.
+     * This method must be registered first to the model.
+     *
+     * @param c             the model change
+     * @param viewSelection the view selection to clear
+     */
+    private void clearViewSelectionUponItemDeletion(
+            final ListChangeListener.Change<? extends ClueViewModel> c,
+            final SelectionModel<ClueViewModel> viewSelection) {
+        while (c.next()) {
+            final boolean removedWithoutReplacement = c.wasRemoved() && !c.wasAdded();
+            final int selectedIndex = viewSelection.getSelectedIndex();
+            final boolean selectionRemoved = selectedIndex >= c.getFrom() &&
+                                             selectedIndex < c.getFrom() + c.getRemovedSize();
+            if (removedWithoutReplacement && selectionRemoved) {
+                viewSelection.clearSelection();
             }
-        });
+        }
+    }
+
+    /**
+     * Updates view across clue selection upon model change.
+     *
+     * @param observable the model observable
+     */
+    private void updateViewAcrossSelectionUponModelSelectionIndexChange(
+            final Observable observable) {
+        updateViewSelectionUponModelSelectionIndexChange(selectedAcrossClueIndex.get(),
+                                                         acrossClueListView.getSelectionModel());
+    }
+
+    /**
+     * Updates view down clue selection upon model change.
+     *
+     * @param observable the model observable
+     */
+    private void updateViewDownSelectionUponModelSelectionIndexChange(final Observable observable) {
+        updateViewSelectionUponModelSelectionIndexChange(selectedDownClueIndex.get(),
+                                                         downClueListView.getSelectionModel());
+    }
+
+    /**
+     * Updates view selection upon model change.
+     *
+     * @param modelNewSelectedIndex the model new selected index
+     * @param viewSelection         the view selection
+     */
+    private void updateViewSelectionUponModelSelectionIndexChange(
+            final int modelNewSelectedIndex,
+            final SelectionModel<ClueViewModel> viewSelection) {
+        if (modelNewSelectedIndex < 0) {
+            viewSelection.clearSelection();
+        } else {
+            viewSelection.select(modelNewSelectedIndex);
+        }
+    }
+
+    /**
+     * Updates the across model selection upon view selection change.
+     *
+     * @param observable the view selection observable
+     */
+    private void updateAcrossModelSelectionUponViewSelectionIndexChange(
+            final Observable observable) {
+        updateModelSelectionUponViewSelectionIndexChange(selectedAcrossClueIndex,
+                                                         acrossClueListView.getSelectionModel(),
+                                                         downClueListView.getSelectionModel());
+    }
+
+    /**
+     * Updates the down model selection upon view selection change.
+     *
+     * @param observable the view selection observable
+     */
+    private void updateDownModelSelectionUponViewSelectionIndexChange(final Observable observable) {
+        updateModelSelectionUponViewSelectionIndexChange(selectedDownClueIndex,
+                                                         downClueListView.getSelectionModel(),
+                                                         acrossClueListView.getSelectionModel());
+    }
+
+    /**
+     * Updates the model selection upon view selection change.
+     *
+     * @param modelSelectedIndex the model selection to update
+     * @param viewSelection      the view selection
+     * @param otherViewSelection the other view selection
+     */
+    private void updateModelSelectionUponViewSelectionIndexChange(
+            final IntegerProperty modelSelectedIndex,
+            final SelectionModel<ClueViewModel> viewSelection,
+            final SelectionModel<ClueViewModel> otherViewSelection) {
+        modelSelectedIndex.set(viewSelection.getSelectedIndex());
+        if (modelSelectedIndex.get() >= 0) {
+            otherViewSelection.clearSelection();
+        }
     }
 }
