dd19b1bf70c5aca796027d88c7929addc577f2d4
Antoine Belvire <antoine.belvire@opensuse.org>
2023-10-22 12:58:10 +0200

    build: Compile Gradle files with target Java 17
    Kotlin 1.9.10 used by Gradle 8.4 does not support compilation with
    target Java 21: Support will arrive with Kotlin 1.9.20. Until Kotlin
    1.9.20 is released and Gradle uses it, use Java 17 as target.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,8 @@
  * SPDX-FileCopyrightText: 2023 Antoine Belvire
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     `kotlin-dsl`
@@ -18,4 +20,16 @@ dependencies {
 repositories {
     mavenCentral()
     gradlePluginPortal()
+}
+
+// This forces compilation of buildSrc with target Java 17, until Kotlin compiler supports Java 21
+// See https://github.com/gradle/gradle/issues/26543.
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    targetCompatibility = "17"
 }
