26c288e5b0f97e344e4d665164396efe7d55e354
Antoine Belvire <antoine.belvire@opensuse.org>
2023-12-30 12:46:12 +0100

    build: Fix catalog name

--- croiseur-web/build.gradle.kts
+++ croiseur-web/build.gradle.kts
@@ -5,15 +5,15 @@
 
 plugins {
     id("re.belv.croiseur.java-application")
-    alias(sbom.plugins.spring.boot)
-    alias(sbom.plugins.spring.deps)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.deps)
 }
 
 dependencies {
     implementation(project(":croiseur"))
-    implementation(sbom.spring.web)
-    implementation(sbom.springdoc.openapi)
-    testImplementation(sbom.spring.tests)
+    implementation(libs.spring.web)
+    implementation(libs.springdoc.openapi)
+    testImplementation(libs.spring.tests)
     runtimeOnly(project(":croiseur-dictionary:croiseur-dictionary-txt-plugin"))
     runtimeOnly(project(":croiseur-solver:croiseur-solver-ginsberg-plugin"))
     runtimeOnly(project(":croiseur-puzzle:croiseur-puzzle-repository-memory-plugin"))
