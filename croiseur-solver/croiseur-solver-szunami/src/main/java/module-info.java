1742b57af4e56e65c85d463b15ab56e41f6c8465
Antoine Belvire <antoine.belvire@opensuse.org>
2025-03-16 22:41:33 +0100

    fix: Fix wasm filler tests

--- croiseur-solver/croiseur-solver-szunami/src/main/java/module-info.java
+++ croiseur-solver/croiseur-solver-szunami/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 Antoine Belvire
+ * SPDX-FileCopyrightText: 2025 Antoine Belvire
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -7,6 +7,7 @@
 module re.belv.croiseur.solver.szunami {
     requires com.fasterxml.jackson.databind;
     requires chicory.sdk;
+    requires chicory.wasm;
 
     exports re.belv.croiseur.solver.szunami;
 }
