ec4c34f79794c155b8e569a6e853a9602c6429e4
Antoine Belvire <antoine.belvire@opensuse.org>
2025-11-03 21:20:52 +0100

    tests(cli): Re-enable module-info

--- croiseur-cli/croiseur-cli-tests/src/test/java/module-info.java
+++ croiseur-cli/croiseur-cli-tests/src/test/java/module-info.java
@@ -1,10 +1,9 @@
 /*
- * SPDX-FileCopyrightText: 2023 Antoine Belvire
+ * SPDX-FileCopyrightText: 2025 Antoine Belvire
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 /** End-to-end tests of Croiseur CLI. */
-// TODO re-enable this (broken by upgrade to junit 5.12.0)
 module re.belv.croiseur.cli.tests {
     requires org.junit.jupiter.api;
     requires re.belv.croiseur.cli;
